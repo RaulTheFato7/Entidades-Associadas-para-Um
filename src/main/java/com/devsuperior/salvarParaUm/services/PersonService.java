7224b0a19a91cb1edf6c64ef4bae9d993f9a1b61
Raul Vitorio <raulvitorio790@gmail.com>
2026-02-20 15:05:51 -0300

    Melhorando o retorno do campo Name

--- src/main/java/com/devsuperior/salvarParaUm/services/PersonService.java
+++ src/main/java/com/devsuperior/salvarParaUm/services/PersonService.java
@@ -3,6 +3,7 @@ package com.devsuperior.salvarParaUm.services;
 import com.devsuperior.salvarParaUm.dto.PersonDepartmentDTO;
 import com.devsuperior.salvarParaUm.entity.Department;
 import com.devsuperior.salvarParaUm.entity.Person;
+import com.devsuperior.salvarParaUm.repositories.DepartmentRepository;
 import com.devsuperior.salvarParaUm.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,16 @@ public class PersonService {
 
     @Autowired
     private PersonRepository repository;
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
     public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
         Person entity = new Person();
         entity.setName(dto.getName());
         entity.setSalary(dto.getSalary());
 
-        Department dept = new Department();
+        Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
         dept.setId(dto.getDepartment().getId());
 
         entity.setDepartment(dept);
